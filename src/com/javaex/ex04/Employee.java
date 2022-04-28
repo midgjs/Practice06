c80679f41ac93637c12cfb369387ab9bf12a9aba
midgjs <sjqksk.10@gmail.com>
2022-04-28 22:32:30 +0900

    2022-04-28

---
+++ src/com/javaex/ex04/Employee.java
@@ -0,0 +1,38 @@
+package com.javaex.ex04;
+
+public class Employee extends User {
+	
+	//필드
+	private int salary;
+	
+	//생성자
+	public Employee() {
+		
+	}
+
+	public Employee(String id, String password, String name, int salary) {
+		super(id, password, name);
+		this.salary = salary;
+	}
+	
+	//메소드 gs
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	//메소드 일반
+	@Override
+	public String toString() {
+		return "Employee [salary=" + salary + "]";
+	}
+	
+	public void showInfo() {
+		System.out.println("#아이디:" + id + ", #패스워드:" + password + ", #이름:" + name + ", #월급:" + salary);
+	}
+
+	
+}
---
+++ src/com/javaex/ex04/UserApp.java
@@ -0,0 +1,19 @@
+package com.javaex.ex04;
+
+public class UserApp {
+
+	public static void main(String[] args) {
+		
+		User uArray[] = new User[3];
+		uArray[0] = new Customer("jws", "j1234", "정우성", 1000);
+		uArray[1] = new Customer("yjs", "y2345", "이효리", 2000);
+		uArray[2] = new Employee("master", "m7788", "운영자", 500000);
+		
+		for(int i=0; i<uArray.length; i++) {
+			uArray[i].showInfo();
+		}
+		System.out.println("운영자의 월급은 " + ???.getSalary() + "원 입니다.");
+		// salary 값 불러내는 방법 ??????????????????????????????????????????????????
+	}
+
+}
